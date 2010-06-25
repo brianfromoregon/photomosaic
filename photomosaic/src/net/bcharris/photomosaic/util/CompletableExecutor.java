4883833d20d1dd46d6077280cf3fe269e28d4eea
brianfromoregon <brianfromoregon@f21d99f2-5b39-0410-bff5-3164207c4f9f>
2010-06-25 02:34:38 +0000

    delete old code base
    git-svn-id: https://photomosaic.googlecode.com/svn/trunk@107 f21d99f2-5b39-0410-bff5-3164207c4f9f

--- photomosaic/src/net/bcharris/photomosaic/util/CompletableExecutor.java
+++
@@ -1,8 +0,0 @@
-package net.bcharris.photomosaic.util;
-
-import java.util.concurrent.Executor;
-
-public interface CompletableExecutor extends Executor
-{
-	public void awaitCompletionAndShutdown();
-}
