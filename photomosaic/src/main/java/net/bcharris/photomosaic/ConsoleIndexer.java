5a20c75d688a69077c60b33f4732127ed355ff4b
brianfromoregon <brianfromoregon@f21d99f2-5b39-0410-bff5-3164207c4f9f>
2010-07-31 03:01:08 +0000

    use args4j for ConsoleIndexer, issue 28
    git-svn-id: https://photomosaic.googlecode.com/svn/trunk@111 f21d99f2-5b39-0410-bff5-3164207c4f9f

--- photomosaic/src/main/java/net/bcharris/photomosaic/ConsoleIndexer.java
+++ photomosaic/src/main/java/net/bcharris/photomosaic/ConsoleIndexer.java
@@ -1,20 +1,50 @@
 package net.bcharris.photomosaic;
 
 import java.io.File;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 public class ConsoleIndexer {
 
-    // This should use args4j: http://code.google.com/p/photomosaic/issues/detail?id=28
+    @Option(name = "-dir", usage = "Source image directory to index (default: working directory)", metaVar = "DIR")
+    private File directoryToIndex = new File(".");
+    @Option(name = "-convert", usage = "ImageMagick convert app", required = true)
+    private File imageMagickConvert = new File("convert");
+    @Option(name = "-w", usage = "Width of indexed images in pixels (default: 16)")
+    private int width = 16;
+    @Option(name = "-h", usage = "Height of indexed images in pixels (default: 11)")
+    private int height = 11;
+
     public static void main(String[] args) {
-        File sourceImageDirectory = new File("E:\\dev\\photomosaic\\image-gen\\colors_16x11");
-        final File convertApp = new File("E:\\Program Files\\ImageMagick-6.5.6-Q16\\convert.exe");
-        final int width = 16;
-        final int height = 11;
+        ConsoleIndexer indexer = new ConsoleIndexer(args);
+        indexer.run();
+    }
+
+    public ConsoleIndexer(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (!directoryToIndex.isDirectory()) {
+                throw new CmdLineException("The specified directory to index is not a directory: " + directoryToIndex.getAbsolutePath());
+            }
+            if (!imageMagickConvert.isFile() || !imageMagickConvert.exists()) {
+                throw new CmdLineException("The specified ImageMagick convert app is invalid: " + imageMagickConvert.getAbsolutePath());
+            }
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            System.exit(1);
+        }
+    }
+
+    private void run() {
         long before = System.currentTimeMillis();
         Indexer indexer = new Indexer();
         File indexFile = Util.createTempFile("photomosaic", ".index");
-        Util.writeIndex(indexer.index(sourceImageDirectory, convertApp, width, height), indexFile);
+        Util.writeIndex(indexer.index(directoryToIndex, imageMagickConvert, width, height), indexFile);
         double secs = (System.currentTimeMillis() - before) / 1000d;
-        Log.log("Finished in " + (int) secs + " seconds, wrote index file to " + indexFile.getAbsolutePath());
+        Log.log("Finished in " + (int) secs + " seconds");
+        Log.log("Wrote index file to " + indexFile.getAbsolutePath());
     }
 }
